0586df7a2be06d12872360498ee12796061dbd52
Chase <cabbott93@gmail.com>
2021-09-23 16:35:00 -0700

    array literal definition

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int [][] numbers = new int[2][3];
-        numbers[0][0] = 1;
+        int [][] numbers = {{2, 3, 4}, {4, 5, 6}};
+
         System.out.println(Arrays.deepToString(numbers));
     }
 }
