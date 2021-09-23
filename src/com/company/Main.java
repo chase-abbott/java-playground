67d3a9c758eaf476be65c20492e91699c47ba214
Chase <cabbott93@gmail.com>
2021-09-23 16:28:25 -0700

    escape characters

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        String message = "Hello World";
-        message.endsWith("!!");
-        System.out.println(message.replace("World", "Chase"));
+        String message = "\\Hello \n\tWorld";
+        System.out.println(message.trim());
     }
 }
