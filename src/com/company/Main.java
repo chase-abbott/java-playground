319b9d310ee2921ba62531755eb980a54afb6800
Chase <cabbott93@gmail.com>
2021-09-23 16:15:18 -0700

    types

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int age = 30;
-        System.out.println((age));
+        byte age = 30;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = true;
     }
 }
