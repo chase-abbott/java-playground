e8f23f68be08b99db8bf5e1ad75d8bbaed7653d9
Chase <cabbott93@gmail.com>
2021-09-23 16:31:49 -0700

    old way of printing arrays

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,14 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        String message = "\\Hello \n\tWorld";
-        System.out.println(message.trim());
+        int [] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[1] = 2;
+
+        System.out.println(Arrays.toString(numbers));
     }
 }
