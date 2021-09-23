104fca0137f7ff1115d7b77033dc07f6db323508
Chase <cabbott93@gmail.com>
2021-09-23 16:34:10 -0700

    2d array print

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,8 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int [] numbers = new int[5];
-        numbers[0] = 1;
-        numbers[1] = 2;
-
-        System.out.println(Arrays.toString(numbers));
+        int [][] numbers = new int[2][3];
+        numbers[0][0] = 1;
+        System.out.println(Arrays.deepToString(numbers));
     }
 }
