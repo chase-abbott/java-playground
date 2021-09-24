373c5f16b72236189d6d76e31006cc9dd4fa17cb
Chase <cabbott93@gmail.com>
2021-09-23 18:34:05 -0700

    mortage calc refactor

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,14 +6,22 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Principal: ");
-        int principal = Integer.parseInt(scanner.next());
+        int principal = scanner.nextInt();
+
         System.out.print("Annual Interest Rate: ");
-        float interest = Float.parseFloat(scanner.next());
+        float interest = scanner.nextFloat();
+        float monthlyInterest = interest / 100 / 12;
+
         System.out.print("Period (Years): ");
-        int years = Integer.parseInt(scanner.next());
+        int years = scanner.nextInt();
+        int months = years * 12;
+
         NumberFormat currency = NumberFormat.getCurrencyInstance();
-        double mortgage = principal * ((((interest / 1000 / 12) * Math.pow((1 + (interest / 1000 / 12)), (years * 12))) / (Math.pow(1 + (interest / 1000 / 12), (years * 12)) - 1))) ;
+        double mortgage = principal *
+                ((monthlyInterest * Math.pow((1 + monthlyInterest), months)) / (Math.pow(1 + monthlyInterest, months) - 1));
+
         String mortgageCurrency = currency.format(mortgage);
         System.out.println("Mortgage:" + mortgageCurrency);
 
