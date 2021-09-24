2e86fadd3bdc97e9787b92e706e8042f9500fe47
Chase <cabbott93@gmail.com>
2021-09-23 18:27:48 -0700

    working mortgage calculator

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,21 @@
 package com.company;
-
-import java.util.Arrays;
+import java.text.NumberFormat;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        int [][] numbers = {{2, 3, 4}, {4, 5, 6}};
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Principal: ");
+        int principal = Integer.parseInt(scanner.next());
+        System.out.print("Annual Interest Rate: ");
+        float interest = Float.parseFloat(scanner.next());
+        System.out.print("Period (Years): ");
+        int years = Integer.parseInt(scanner.next());
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        double mortgage = principal * ((((interest / 1000 / 12) * Math.pow((1 + (interest / 1000 / 12)), (years * 12))) / (Math.pow(1 + (interest / 1000 / 12), (years * 12)) - 1))) ;
+        String mortgageCurrency = currency.format(mortgage);
+        System.out.println("Mortgage:" + mortgageCurrency);
 
-        System.out.println(Arrays.deepToString(numbers));
     }
 }
